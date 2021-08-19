6e3a3b978913231d1171c804e9ff0e21f9953c0d
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-19 10:33:17 +0300

    Brew formula update for datree-cli version 0.6.5-DAT-3371-homebrew

--- datree.rb
+++ datree.rb
@@ -5,23 +5,23 @@
 class Datree < Formula
   desc ""
   homepage ""
-  version "0.6.4-DAT-3371-homebrew"
+  version "0.6.5-DAT-3371-homebrew"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/datreeio/datree/releases/download/0.6.4-DAT-3371-homebrew/datree-cli_0.6.4-DAT-3371-homebrew_Darwin_x86_64.zip"
-    sha256 "0b708af74c6a8fc2af841e10c640aa1f1ab56d7fb2a9a56ff38f1ae86170194e"
+    url "https://github.com/datreeio/datree/releases/download/0.6.5-DAT-3371-homebrew/datree-cli_0.6.5-DAT-3371-homebrew_Darwin_x86_64.zip"
+    sha256 "1d2b3b52646326ca95654b62376e56d45bf5b3e3ba9d29487e4b3573e4a858f4"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.6.4-DAT-3371-homebrew/datree-cli_0.6.4-DAT-3371-homebrew_Linux_x86_64.zip"
-    sha256 "51b6c042de4720a5c2e3f799e4d476af3f5950742bc5be779583d92e46eaf320"
+    url "https://github.com/datreeio/datree/releases/download/0.6.5-DAT-3371-homebrew/datree-cli_0.6.5-DAT-3371-homebrew_Linux_x86_64.zip"
+    sha256 "effd9aa34c6d8511402ca1ebd267235a69f52dbc16d9f5c8c1218b53382aa2b2"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/0.6.4-DAT-3371-homebrew/datree-cli_0.6.4-DAT-3371-homebrew_Linux_arm64.zip"
-    sha256 "46f456c2fbff3739eba72c1056fc0ece5afa7bcc05ac110ed947da293b29aa8a"
+    url "https://github.com/datreeio/datree/releases/download/0.6.5-DAT-3371-homebrew/datree-cli_0.6.5-DAT-3371-homebrew_Linux_arm64.zip"
+    sha256 "d18e60373fd490fa5f9838b0276aad0b06a75fb0bf578f9427678a24675961cd"
   end
 
   def install
-    bin.install "datree-cli"
+    bin.install "datree"
   end
 end
