8e61427e4a8bf95947e5c2e874f1e78be5c64d61
Travis CI <builds@travis-ci.com>
2022-03-23 12:58:54 +0000

    Brew formula update for datree-cli version 1.0.15

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "1.0.6"
+  version "1.0.15"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.0.6/datree-cli_1.0.6_Darwin_x86_64.zip"
-    sha256 "c9da2630bd04282370b9d427ab26cea6a447f3ec99c4196bfa33d24987a81711"
+    url "https://github.com/datreeio/datree/releases/download/1.0.15/datree-cli_1.0.15_Darwin_x86_64.zip"
+    sha256 "b81778dfc1fd66850118765366f8545640d3894667886491966bf2a5b1ce0689"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/1.0.6/datree-cli_1.0.6_Darwin_arm64.zip"
-    sha256 "c8989326d2e80873c322a45653cbb4da7dc6b16d775ea5f293b0c32c46656706"
+    url "https://github.com/datreeio/datree/releases/download/1.0.15/datree-cli_1.0.15_Darwin_arm64.zip"
+    sha256 "0766b4d9c5fc5c092bb5afbf15690d0ec631081dd95455ba1973dbca0aa47505"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.0.6/datree-cli_1.0.6_Linux_x86_64.zip"
-    sha256 "1b3a4f97ab1daa15a1e773a85856f3c98db80c433c4a2b3b1c8d51431a38070a"
+    url "https://github.com/datreeio/datree/releases/download/1.0.15/datree-cli_1.0.15_Linux_x86_64.zip"
+    sha256 "53e5a042848b9f603cf43bc94154fd8230b4332d0b73ddba6e00ac67985e02d0"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/1.0.6/datree-cli_1.0.6_Linux_arm64.zip"
-    sha256 "d00eb5584c8b38342ea901134da559c72cede597ef2b381ea622a300044a994b"
+    url "https://github.com/datreeio/datree/releases/download/1.0.15/datree-cli_1.0.15_Linux_arm64.zip"
+    sha256 "120b0e9227dc3dbbe3926aada24731a6ce991c8a45a3a62cc02ece74b88fee9f"
   end
 
   def install
